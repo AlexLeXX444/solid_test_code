fcf8d60c82efd9df091ae4fd44e30e9b62d88d2f
Alex <alexlexx444@yandex.ru>
2022-09-26 23:51:34 +0500

    fill main class

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -5,24 +5,98 @@ import Technics.Bike.*;
 import Technics.Helicopter.*;
 import Technics.Jetski.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Technics n1;
-        n1 = new Jetski(
-                "BMV",
-                "n1",
-                123,
+        List<Technics> technics = new ArrayList<>();
+
+        technics.add(new Atv(
+                "Honda",
+                "Honda Talon 1000X",
+                123456789,
                 0,
-                "atm",
-                123,
-                321,
-                100,
-                156,
-                26,
-                "123",
-                124,
+                "атмосферный двигатель",
+                1234566789,
+                80,
+                240,
+                350,
+                0,
+                "цепной",
+                false,
                 false
-        );
-        System.out.println(n1);
+        ));
+
+        technics.add(new Avto(
+                "Ford",
+                "Ford Fiesta",
+                1234567891,
+                3,
+                "атмосферный двигатель",
+                123456-456789,
+                1600,
+                940,
+                1550,
+                75,
+                "переднеприводный",
+                "универсал"
+        ));
+
+        technics.add(new Bike(
+                "KAWASAKI",
+                "KAWASAKI NINJA 400",
+                1234567891,
+                1,
+                "атмосферный двигатель",
+                1234456789,
+                750,
+                160,
+                240,
+                0,
+                "цепной",
+                false,
+                false
+        ));
+
+        technics.add(new Helicopter(
+                "AirScooter Corporation",
+                "Air Scooter 2",
+                1234567891,
+                0,
+                "атмосферный двигатель",
+                1234456789,
+                500,
+                140,
+                290,
+                0,
+                "сталь",
+                "углепластик",
+                3000
+        ));
+
+        technics.add(new Jetski(
+                "YAMAHA",
+                "YAMAHA FX CRUISER SVHO",
+                1234567891,
+                2,
+                "атмосферный двигатель",
+                1234456789,
+                300,
+                397,
+                550,
+                0,
+                "углепластик",
+                2,
+                true
+        ));
+
+        for (int i = 0; i < technics.size(); i++) {
+            System.out.println(technics.get(i));
+            System.out.println("\t\t" + technics.get(i).getInfoTechnics());
+            System.out.println("\t\t" + technics.get(i).getInfoMass());
+            System.out.println("\t\t" + technics.get(i).getInfoPowerPlant());
+            System.out.println();
+        }
     }
 }
