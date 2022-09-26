04c046128c365834c887b324a43ce4094718e3f0
Alex <alexlexx444@yandex.ru>
2022-09-26 15:44:25 +0500

    refactor Technics out console interfaces

---
+++ Java/src/Interfaces/TechnicsString.java
@@ -0,0 +1,8 @@
+package Interfaces;
+
+public interface TechnicsString {
+    /**
+     * toString - переопределяем метод и выводит "красивую" строку с описанием техники.
+     */
+    String toString();
+}
