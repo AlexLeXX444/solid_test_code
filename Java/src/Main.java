70cde0d3b4036a1d6483af95524536ef36139e55
Alex <alexlexx444@yandex.ru>
2022-09-26 15:46:06 +0500

    make finally class Avto

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,24 @@
+import Technics.Technics;
+import Technics.Atv.*;
+import Technics.Avto.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Technics n1;
+        n1 = new Avto(
+                "BMV",
+                "n1",
+                123,
+                0,
+                "atm",
+                123,
+                321,
+                100,
+                156,
+                26,
+                "переднеприводный",
+                "седан"
+        );
+        System.out.println(n1);
     }
 }
--- Java/src/Technics/Avto/Avto.java
+++ Java/src/Technics/Avto/Avto.java
@@ -2,7 +2,7 @@ package Technics.Avto;
 
 import Technics.Technics;
 
-public class Avto extends Technics {
+public class Avto extends Technics implements Interfaces.TechnicsString{
     /**
      * chassisType - тип шасси.
      * bodyType - тип кузова.
@@ -10,7 +10,24 @@ public class Avto extends Technics {
     protected String chassisType;
     protected String bodyType;
 
-    public Avto(String kindOfTechnic, String technicMark, String technicModel, int technicSerialNumber, int passengersNum, String powerPlant, int powerPlantSerialNumber, int powerPlantRangeReserve, double massMinimum, double massMaximum, double massLoadCapacity) {
-        super(kindOfTechnic, technicMark, technicModel, technicSerialNumber, passengersNum, powerPlant, powerPlantSerialNumber, powerPlantRangeReserve, massMinimum, massMaximum, massLoadCapacity);
+    public Avto(String technicMark, String technicModel, int technicSerialNumber,
+                int passengersNum,
+                String powerPlant, int powerPlantSerialNumber, int powerPlantRangeReserve,
+                double massMinimum, double massMaximum, double massLoadCapacity,
+                String chassisType, String bodyType
+    ) {
+        super("Automobile",
+                technicMark, technicModel, technicSerialNumber,
+                passengersNum,
+                powerPlant, powerPlantSerialNumber, powerPlantRangeReserve,
+                massMinimum, massMaximum, massLoadCapacity);
+        this.chassisType = chassisType;
+        this.bodyType = bodyType;
+    }
+
+    @Override
+    public String toString() {
+        String strOut = String.format("Автомобиль марки %s модель %s ", super.technicMark, super.technicModel);
+        return strOut + String.format("%s %s.", chassisType, bodyType);
     }
 }
