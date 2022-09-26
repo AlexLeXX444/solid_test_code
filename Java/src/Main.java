562966d41c10cf4f1003cb231d966613954be082
Alex <alexlexx444@yandex.ru>
2022-09-26 17:13:59 +0500

    make finally class Bike

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,11 +1,12 @@
 import Technics.Technics;
 import Technics.Atv.*;
 import Technics.Avto.*;
+import Technics.Bike.*;
 
 public class Main {
     public static void main(String[] args) {
         Technics n1;
-        n1 = new Avto(
+        n1 = new Bike(
                 "BMV",
                 "n1",
                 123,
@@ -16,8 +17,9 @@ public class Main {
                 100,
                 156,
                 26,
-                "переднеприводный",
-                "седан"
+                "цепной",
+                false,
+                true
         );
         System.out.println(n1);
     }
--- Java/src/Technics/Bike/Bike.java
+++ Java/src/Technics/Bike/Bike.java
@@ -1,8 +1,9 @@
 package Technics.Bike;
 
+import Interfaces.TechnicsString;
 import Technics.Technics;
 
-public class Bike extends Technics {
+public class Bike extends Technics implements TechnicsString {
     /**
      * rearWheelDriveType - Тип привеода заднего колеса.
      * luggageCompartment - Наличие багажного отсека.
@@ -12,7 +13,35 @@ public class Bike extends Technics {
     protected boolean luggageCompartment;
     protected boolean attachmentSidecar;
 
-    public Bike(String kindOfTechnic, String technicMark, String technicModel, int technicSerialNumber, int passengersNum, String powerPlant, int powerPlantSerialNumber, int powerPlantRangeReserve, double massMinimum, double massMaximum, double massLoadCapacity) {
-        super(kindOfTechnic, technicMark, technicModel, technicSerialNumber, passengersNum, powerPlant, powerPlantSerialNumber, powerPlantRangeReserve, massMinimum, massMaximum, massLoadCapacity);
+    public Bike(String technicMark, String technicModel, int technicSerialNumber,
+                int passengersNum,
+                String powerPlant, int powerPlantSerialNumber, int powerPlantRangeReserve,
+                double massMinimum, double massMaximum, double massLoadCapacity,
+                String rearWheelDriveType, boolean luggageCompartment, boolean attachmentSidecar
+    ) {
+        super("Bike",
+                technicMark, technicModel, technicSerialNumber,
+                passengersNum,
+                powerPlant, powerPlantSerialNumber, powerPlantRangeReserve,
+                massMinimum, massMaximum, massLoadCapacity);
+        this.rearWheelDriveType = rearWheelDriveType;
+        this.luggageCompartment = luggageCompartment;
+        this.attachmentSidecar = attachmentSidecar;
+    }
+
+    @Override
+    public String toString() {
+        String strOut = String.format("Мотоцикл марки %s модель %s ", super.technicMark, super.technicModel);
+        strOut += String.format("имеющий %s привод", rearWheelDriveType);
+        if (luggageCompartment && attachmentSidecar) {
+            return strOut + String.format(" с багажником и креплением для коляски.");
+        }
+        else if (luggageCompartment) {
+            return strOut + String.format(" с багажником.");
+        }
+        else if (attachmentSidecar) {
+            return strOut + String.format(" с креплением для коляски.");
+        }
+        return strOut + ".";
     }
 }
